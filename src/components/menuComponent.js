29f2f59ba4eed431a2b29127f0b84bc9103d6389
ashleshshenoy <ashleshshenoy09@gmail.com>
2021-12-19 20:07:28 +0530

    ui improvement

--- src/components/menuComponent.js
+++ src/components/menuComponent.js
@@ -8,11 +8,13 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
         super(props);
 
         this.state = {
+            commentHeader : null,
             selectedDish: null
         }
     }
 
     onDishSelect(dish) {
+        this.setState({ commentHeader: "Comments"})
         this.setState({ selectedDish: dish});
     }
 
@@ -35,16 +37,19 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
     }
     renderComments(dish){
       if (dish != null){
-      return(
-          dish.comments.map((comment)=>{
+        return(
+              
+              dish.comments.map((comment)=>{
               return(
-                <div>
-                  <p>{comment.author}</p>
+                
+                <div key={comment.id} className="p-4 border b-1 ">
+                  <h4 className="text-secondary">{comment.author}</h4>
                   <p>{comment.comment}</p>
+                  <small className="text-muted float-right ">{comment.date}</small>
               </div>
               )
           })
-      )
+      ) 
     }
     else{
       return(<div></div>);
@@ -54,12 +59,12 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-              <div  className="col-12 col-md-5 m-1">
+              <div  className="col-12 col-md-5 m-1 ">
                 <Card key={dish.id}
                   onClick={() => {this.onDishSelect(dish);}}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
+                      <CardTitle className="text-dark bg-light border b-2 border-dark p-3  w-50 rounded"><h4>{dish.name}</h4></CardTitle>
                   </CardImgOverlay>
                 </Card>
               </div>
@@ -76,6 +81,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
                     {this.renderDish(this.state.selectedDish)}
                   </div>
                   <div  className="col-12 col-md-5 m-1">
+                  <h3 className="text-primary">{this.state.commentHeader}  </h3>
                     {this.renderComments(this.state.selectedDish)}
                   </div>
                 </div>
